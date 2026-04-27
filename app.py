c9b0dec889861b3f884d7bda0048583d05ff0fab
Shubham Raj <shubmrj@gmail.com>
2026-04-27 12:04:52 +0530

    tried to resolved issue of deployed model~14

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 from flask import Flask, request, jsonify, render_template
-import os
-import sys
-
-# NumPy 1.x vs 2.x Compatibility Hack
-import numpy as np
-print(f"DEBUG: NumPy version: {np.__version__}")
-try:
-    import numpy._core as _core
-except ImportError:
-    # If we are on NumPy 1.x, alias _core to core
-    try:
-        import numpy.core as _core
-        sys.modules['numpy._core'] = _core
-        print("DEBUG: Aliased numpy.core to numpy._core")
-    except ImportError:
-        pass
-
 import joblib
 import pandas as pd
 import numpy as np
 import shap
 import os
+import sys
 import traceback
 import tempfile
 
